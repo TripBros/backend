a3043e1b7fa55ac4350c89a2f347010339d8e096
eekrwl <wani9909@naver.com>
2023-07-31 10:43:13 +0900

    first commit

---
+++ src/main/java/com/example/tripbros/TripbrosApplication.java
@@ -0,0 +1,13 @@
+package com.example.tripbros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripbrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripbrosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tripbros/TripbrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tripbros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripbrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
